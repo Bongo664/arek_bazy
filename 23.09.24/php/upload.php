422f20497ac24d9c3f32869413683fe646aa9def
Bongo664 <patrykpojda@gmail.com>
2024-09-23 10:32:53 +0200

    wstępne tworzenie pliku upload.php

--- 23.09.24/php/upload.php
+++ 23.09.24/php/upload.php
@@ -1,4 +1,10 @@
 <?php
 session_start();
-if(!isset($_SESSION[]))
+if(!isset($_SESSION['user_id'])) {
+    header("Location: ../login.php");
+    exit();
+}
+include('../includes/db.php');
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $user_id = $_SESSION['user_id'];
 ?>
