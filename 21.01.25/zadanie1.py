ad244e1ec6171311cfce56f56d4834c0c2e3ce34
Bongo664 <patrykpojda@gmail.com>
2025-01-21 08:42:28 +0100

    cwiczenia py

---
+++ 21.01.25/zadanie1.py
@@ -0,0 +1,9 @@
+'''
+ilosc = 12
+napis = "W magazynie" + str(ilosc) + 'sztuk'
+'''
+wiek = 10
+adres= 'pszczyna ul poniatowskiego'
+nr = 2
+napis = 'fajnie mieć {}'.format(wiek)
+print(napis)
---
+++ 21.01.25/zadanie3.py
@@ -0,0 +1,22 @@
+'''
+while True:
+    print("działą...")
+'''
+counter = 1
+'''
+while counter < 10:
+    print(counter)
+    counter += 1
+
+'''
+'''
+while counter <= 10:
+    counter +=1
+    if counter % 2 == 0:
+        continue
+    print(counter)'''
+while counter <= 10:
+    counter +=1
+    if counter == 5:
+        break
+    print(counter)
