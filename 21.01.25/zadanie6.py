5388c3453599c5748999298ecf75c8deddc0a6d1
Bongo664 <patrykpojda@gmail.com>
2025-03-03 17:25:15 +0100

    Refactor zadanie6.py to remove commented code and update list comprehension range

--- 21.01.25/zadanie6.py
+++ 21.01.25/zadanie6.py
@@ -1,5 +1,5 @@
 #listy - możemy modyfikować, są mutowalne
-'''
+
 mojalista = [1,2,3,4,5,6,'S','a','arek',True,False]
 print(mojalista[4])
 mojalista[0] = 25
@@ -8,15 +8,14 @@ mojalista.append('lubie programować')
 print(mojalista)
 print(mojalista[0:5])
 print(mojalista[5:])
-'''
-'''
 lista1 = [1,2,3,4,5]
 lista2 = [2,3]
 lista3 = lista1 + lista2
 print(lista3)
 print(3 in lista3)
 print(len(lista3))
-'''
+
 mojasuperlista = [zmienna for zmienna in range(10)]
 print(mojasuperlista)
-mojasuperlista = [zmienna for zmienna in range]
+mojasuperlista = [zmienna for zmienna in range(20)]
+print(mojasuperlista)
